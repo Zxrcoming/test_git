698fbee895e3dcd27092c28a25d3efbf842db3ae
zxrcoming <2536914044qq.com>
2020-03-17 09:41:43 +0800

    added a new file.

---
+++ 1.js
@@ -0,0 +1,32 @@
+// some the about the performance of Magit.
+// this is master branch.
+const navbarEl = document.querySelector(".navbar");
+const headerEl = document.querySelector(".header-desc");
+
+const burgerEl = document.querySelector(".burger");
+const menusEl = document.querySelector(".menus");
+
+window.addEventListener("load", () => {
+  headerEl.style.transform = "translateY(200px)";
+  headerEl.style.opacity = "1";
+});
+
+window.addEventListener("scroll", () => {
+  let height = navbarEl.getBoundingClientRect().height;
+  menusEl.style.display = "";
+  if (window.pageYOffset - height > 600) {
+    if (!navbarEl.classList.contains("sticky")) {
+      navbarEl.classList.add("sticky");
+    }
+  } else {
+    navbarEl.classList.remove("sticky");
+  }
+});
+console.log(menusEl);
+burgerEl.addEventListener("click", () => {
+  if (menusEl.style.display === "") {
+    menusEl.style.display = "block";
+  } else {
+    menusEl.style.display = "";
+  }
+});
