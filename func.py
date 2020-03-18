f93128c59135cf45bb5d3c611054adddaf2749b4
zxrcoming <2536914044qq.com>
2020-03-18 09:45:53 +0800

    learning spacemacs basics.

--- func.py
+++ func.py
@@ -1,4 +1,26 @@
 import sys
+
+
+def testafaf():
+    return "hello"
+
+
+def tesfaft():
+    return "hello"
+
+
+def ljltest():
+    return "hello"
+
+
+def tsfakkkest():
+    return "hello"
+
+
+def fatest():
+    return "hello"
+
+
 print(sys.version_info)
 for i in range(19):
     print("hello I will use the spacemacs")
